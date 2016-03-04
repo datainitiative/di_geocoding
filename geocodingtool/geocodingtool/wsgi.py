fa995844e02e1b9bc7f24f82b1eb8641a9adeb2d
Qing Liu <Qing.Liu@du.edu>
2016-03-03 17:05:17 -0700

    change wsgi settings for multiple django sites

--- geocodingtool/geocodingtool/wsgi.py
+++ geocodingtool/geocodingtool/wsgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os, sys
 sys.path.append('/django-apps/di_geocoding/geocodingtool/')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/django-apps/di_geocoding/geocodingtool/egg_cache")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geocodingtool.settings")
+# Use this for single Django site in a single mod_wsgi process
+#os.environ.setdefault("PYTHON_EGG_CACHE", "/django-apps/di_geocoding/geocodingtool/egg_cache")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geocodingtool.settings")
+# Use this for multiple Django site in a single mod_wsgi process
+os.environ["PYTHON_EGG_CACHE"] = "/django-apps/di_geocoding/geocodingtool/egg_cache"
+os.environ["DJANGO_SETTINGS_MODULE"] = "geocodingtool.settings"
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
